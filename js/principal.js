2daea3d586274ac5be224a95b3ee319c6b064645
Beatriz Lima <bialima0409@outlook.com>
2026-07-30 20:12:17 -0300

    Calcula imc de todos os pacientes e adiciona style para erros de dados iválidos

--- js/principal.js
+++ js/principal.js
@@ -1,31 +1,39 @@
 var titulo = document.querySelector(".titulo");
 titulo.textContent = "Aparecida Nutricionista";
 
-var paciente = document.querySelector("#primeiro-paciente");
-var tbPeso = paciente.querySelector(".info-peso");
-var tbAltura = paciente.querySelector(".info-altura");
-var tbImc = paciente.querySelector(".info-imc"); 
+var pacientes = document.querySelectorAll(".paciente");
 
-var peso = tbPeso.textContent;
-var altura = tbAltura.textContent;
+for (var i =0 ; i < pacientes.length; i++) {
+    var paciente = pacientes[i];
 
-var pesoEhValido = true;
-var alturaEhValida = true;
+    var tbPeso = paciente.querySelector(".info-peso");
+    var tbAltura = paciente.querySelector(".info-altura");
+    var tbImc = paciente.querySelector(".info-imc"); 
 
-if (peso <= 0 || peso >= 1000) {
-    tbImc.textContent = "Peso inválido!";
-    pesoEhValido = false;
+    var peso = tbPeso.textContent;
+    var altura = tbAltura.textContent;
+
+    var pesoEhValido = true;
+    var alturaEhValida = true;
+
+    if (peso <= 0 || peso >= 1000) {
+        tbImc.textContent = "Peso inválido!";
+        pesoEhValido = false;
+        paciente.classList.add("paciente-invalido");
+    }
+
+    if (altura <= 0 || altura >= 1000) {
+        tbImc.textContent = "Altura inválida!";
+        alturaEhValida = false;
+        paciente.classList.add("paciente-invalido");
+    }
+
+    if (pesoEhValido && alturaEhValida) {
+        var imc = peso / (altura * altura);
+        tbImc.textContent = imc.toFixed(2);
+    }
 }
 
-if (altura <= 0 || altura >= 1000) {
-    tbImc.textContent = "Altura inválida!";
-    alturaEhValida = false;
-}
-
-if (pesoEhValido && alturaEhValida) {
-    var imc = peso / (altura * altura);
-    tbImc.textContent = imc;
-}
 
 
 
